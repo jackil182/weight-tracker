e712970548348e4b5d2b57db56e11be80967f5de
Nazarii Lavrenchuk <jackil182@gmail.com>
2019-03-15 18:30:03 +0200

    Updates

---
+++ precache-manifest.a3d39d8b06c5cfd97f6f1600579c7267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79d0760f50ad77406390",
+    "url": "/static/js/main.687fed19.chunk.js"
+  },
+  {
+    "revision": "86b4a99ce80109642106",
+    "url": "/static/js/2.c9b54819.chunk.js"
+  },
+  {
+    "revision": "79d0760f50ad77406390",
+    "url": "/static/css/main.456f37ff.chunk.css"
+  },
+  {
+    "revision": "86b4a99ce80109642106",
+    "url": "/static/css/2.1227562d.chunk.css"
+  },
+  {
+    "revision": "e6a22644f9f8e621e552bbefb010d09d",
+    "url": "/index.html"
+  }
+];
